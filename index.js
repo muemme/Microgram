911ba40996f9510209e3d3b07bb62b0abe236c3c
Florian Stahr <mail@florianstahr.com>
2018-12-18 23:55:52 +0100

    ADD: next to redirect

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ const httpsOptions = {
   ca,
 };
 
-app.get('*', function(req, res) {  
+app.get('*', function(req, res, next) {  
   if (!req.secure) {
     res.redirect('https://' + req.headers.host + req.url);
+  } else {
+    next();
   }
 });
 
